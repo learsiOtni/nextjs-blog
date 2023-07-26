d157b291affeafac78343ac425f4422edd25b8df
learsiOtni <learsiOtni@gmail.com>
2023-07-26 21:15:43 +0100

    Changed hello api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 
 
 export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello World there is new changes!!!!' });
+    res.status(200).json({ text: 'Hello World there is new changes!!!! FINAL CHANGES' });
 }
